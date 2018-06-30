e6f62b32b8f612a3da06a77b29ca0b9061c22ad6
Jack Park <jackpark@gmail.com>
2018-06-29 19:19:42 -0700

    bug fixed

--- src/pages/QuestView.vue
+++ src/pages/QuestView.vue
@@ -17,14 +17,9 @@ export default {
   mounted () {
     const id = this.$route.params.id
     const quests = api.service('quests')
-    quests.find({ 'id': id })
+    quests.find({ query: { 'id': id } })
       .then((response) => {
         console.log(response)
-        // TODO this is a hack to pluck a hit out of 5 hits
-        // which means that query is not working.
-        // A stored quest looks like this:
-        // {"id":"e601cafc-7565-4058-aec3-229be2eada26","label":"Another test","details":"Yup","_id":"RFNf5Pb7CNG9vN6S"}
-        // and the query against 'id' seems to be failing
         var x = response.data[0]
         this.$data.label = x.label
         this.$data.details = x.details
