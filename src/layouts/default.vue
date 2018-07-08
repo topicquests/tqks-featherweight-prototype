9ad5a98fbd06805871ba087cc84c5f42a49123b8
Jack Park <jackpark@gmail.com>
2018-07-07 19:40:52 -0700

    bug fix

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -164,7 +164,7 @@ export default {
       this.setUser(user)
       this.$data.isAuthenticated = true
       this.checkAdmin()
-      this.$router.push({ name: 'home' })
+      // this.$router.push({ name: 'home' })
     })
 
     // On logout
