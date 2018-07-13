d341413a77e61fd11fa2ab6c160f2ac71bcdb9d0
Eyasu Kifle <kpsychwave@gmail.com>
2018-07-13 03:30:19 +0000

    #13 Add client-side pagination

--- src/pages/Quests.vue
+++ src/pages/Quests.vue
@@ -1,75 +1,134 @@
 <template>
-  <q-page>
-        <h4><img style="margin-right:4px;" src="statics/images/ibis/map.png">Quests</h4>
-        <q-btn v-if="isAuthenticated" style="margin-bottom: 4px;" label="New Quest" @click="$router.replace('/questedit')" />
-        <q-scroll-area style="width: 800px; height: 400px;  margin-top: 1px;">
-        <table class="q-table">
-          <tbody >
-            <tr v-for="quest in serverData" :key="quest.id">
-              <td class="text-left"><img :src="quest.imgsm"></td>
-              <td class="text-left"><a :href="`/index.html#/questview/${quest.id}`">{{ quest.label }}</a></td>
-              <td class="text-left">{{ quest.handle }}</td>
-              <td class="text-left">{{ quest.date }}</td>
-            </tr>
-          </tbody>
-        </table>
-        </q-scroll-area>
+  <q-page :padding="true">
+          <q-btn style="margin-bottom: 4px;" label="New Quest" @click="$router.replace('/questedit')" />
+
+        <q-table
+          title="My Quests"
+          :data="serverData"
+          :columns="columns"
+        >
+       <template slot="body" slot-scope="props">
+          <q-tr :props="props">
+               <q-td  key="type" style="width: 30px"  :props="props">
+                      <i :class="'ibis-icon ibis-' + props.row.type"/>
+                    </q-td>
+                <q-td key="label" :props="props">
+                  {{props.row.label}}
+                </q-td>
+                <q-td key="handle" :props="props">
+                  {{props.row.handle}}
+                </q-td>
+                <q-td key="date" :props="props">
+                  {{props.row.date}}
+                </q-td>
+                <q-td key="id" auto-width :props="props">
+                  <router-link :to="{ name: 'questview', params: { id:  props.row.id }}">View</router-link>
+                </q-td>
+        </q-tr>
+        </template>
+    
+             
+            </q-table>
   </q-page>
 </template>
 
 <script>
-import api from 'src/api'
-const conversation = api.service('conversation')
+  import api from 'src/api'
+  const conversation = api.service('conversation')
 
-export default {
-  props: [ 'user' ],
-  data () {
-    return {
-      isAuthenticated: false,
-      serverPagination: {},
-      serverData: []
-    }
-  },
-  methods: {
-    fill (n) {
-      var jsx = {}
-      jsx.imgsm = n.imgsm
-      jsx.label = `<router-link :to="{ name: 'questview', params: { id: ${ n.id }}">${ n.label }</router-link>`
-      jsx.creator = n.creator
-      jsx.handle = n.handle
-      jsx.date = n.date
-      this.$data.serverData.push(jsx)
-    },
-    request ({ pagination }) {
-      //let skip = this.data.
-      this.$store.dispatch('conversation/find', { 
-        query: {
-          $sort: {
-            date: -1
+  export default {
+    props: ['user'],
+    data() {
+      return {
+        columns: [
+          {
+            name: 'type',
+            required: true,
+            label: 'Type',
+            align: 'left',
+            field: 'type',
+            sortable: true
           },
-          'type': 'map'
-        }} )
-        .then((response) => {
-          this.$data.serverPagination = pagination
-          var data = response.data
-          // alert(JSON.stringify(data))
-          if (data && data.length > 0) {
-            this.$data.serverData = data
-            // data.map(this.fill)
-          }
-        })
+          {
+            name: 'label',
+            required: true,
+            label: 'Label',
+            align: 'left',
+            field: 'label',
+            sortable: true
+          },
+          {
+            name: 'handle',
+            required: true,
+            label: 'Handle',
+            align: 'left',
+            field: 'handle',
+            sortable: true
+          },
+          {
+            name: 'date',
+            required: true,
+            label: 'Date',
+            align: 'left',
+            field: 'date',
+            sortable: true
+          },
+          {
+            name: 'id',
+            required: true,
+            label: 'Action',
+            align: 'left',
+            field: 'id',
+            sortable: true
+          },
+        ],
+        isAuthenticated: true,
+        serverPagination: {},
+        serverData: []
       }
-  },
-  mounted () {
-    if (this.user) {
-      this.$data.isAuthenticated = true
+    },
+    methods: {
+      fill(n) {
+        var jsx = {}
+        jsx.imgsm = n.imgsm
+        jsx.label = `<router-link :to="{ name: 'questview', params: { id: ${ n.id }}">${ n.label }</router-link>`
+        jsx.creator = n.creator
+        jsx.handle = n.handle
+        jsx.date = n.date
+        this.$data.serverData.push(jsx)
+      },
+      request({ pagination }) {
+        //let skip = this.data.
+        this.$store.dispatch('conversation/find', {
+            query: {
+              $limit: 100,
+              $sort: {
+                date: -1
+              },
+              'type': 'map'
+            }
+          })
+          .then((response) => {
+            this.$data.serverPagination = pagination
+            var data = response.data
+            // alert(JSON.stringify(data))
+            if (data && data.length > 0) {
+              this.$data.serverData = data
+              // data.map(this.fill)
+            }
+          })
+      }
+    },
+    mounted() {
+      if (this.user) {
+        this.$data.isAuthenticated = true
+      }
+      this.request({
+        pagination: this.serverPagination
+      })
+
     }
-    this.request({
-      pagination: this.serverPagination
-    })
-    
   }
-}
 </script>
 
 <style>
