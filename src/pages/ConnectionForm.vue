b10e4b4bf908e78281699d9d9b14b3f4f7a70b59
Jack Park <jackpark@gmail.com>
2019-01-30 10:47:09 -0800

    starting connections UX

---
+++ src/pages/ConnectionForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page :padding="true">
+    <h6>New Connection Form</h6>
+    <!-- TODO a massive collection of pull-down selections -->
+  </q-page>
+</template>
+<script>
+export default {
+  props: [ 'user' ],
+  validations: {
+    label: { required }
+  },
+  data () {
+    return {
+      source: '',
+      target: '',
+      callerId: '',
+      callerType: ''
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  }
+</script>
+<style>
+</style>
