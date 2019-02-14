d584d19febbdf0b58c38840e208d35afbd4b78e7
Eyasu Kifle <eyasu.kifle@gmail.com>
2019-02-14 07:24:38 +0000

    Clean up: remove typo service

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -2,7 +2,6 @@ const users = require("./users/users.service.js");
 const channels = require("./channels/channels.service.js");
 const conversation = require("./conversation/conversation.service.js");
 const history = require("./history/history.service.js");
-const invintations = require("./invintations/invintations.service.js");
 const messages = require("./messages/messages.service.js");
 const tags = require("./tags/tags.service.js");
 const mailer = require("./mailer/mailer.service.js");
@@ -18,7 +17,6 @@ module.exports = function(app) {
   app.configure(channels);
   app.configure(conversation);
   app.configure(history);
-  app.configure(invintations);
   app.configure(messages);
   app.configure(tags);
   app.configure(mailer);
