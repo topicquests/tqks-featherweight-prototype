3b4c95f31f9417d13426c9d23209575b6156d9c9
Eyasu <eyasu.kifle@gmail.com>
2019-02-21 19:17:19 -0800

    Enhancement: HTML email template for User Activation #62 - added basic HTML template

--- api/src/services/authmanagement/notifier.js
+++ api/src/services/authmanagement/notifier.js
@@ -1,6 +1,24 @@
+const Email = require('email-templates');
+
 module.exports = function(app) {
   const emailConfig = app.get("emailconfig");
-  const baseURL = app.get("baseURL");
+
+  const {
+    SMTP_HOST = 'smtp.mailtrap.io',
+    SMTP_PORT = 2525,
+    SMTP_USER = 'af9a8766d909b7',
+    SMTP_PASS = 'd803d787c8349d',
+  } = emailConfig;
+  
+  const transport = {
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    auth: {
+      user: SMTP_USER,
+      pass: SMTP_PASS
+    }
+  };
+  const baseURL = app.get('baseURL');
   function getLink(type, hash) {
     const url = `${baseURL}/token/${type}/${hash}`;
     return url;
@@ -8,73 +26,103 @@ module.exports = function(app) {
 
   function sendEmail(email) {
     return app
-      .service("mailer")
+      .service('mailer')
       .create(email)
       .then(function(result) {
-        console.log("Sent email", result);
+        console.log('Sent email', result);
       })
       .catch(err => {
-        console.log("Error sending email", err);
+        console.log('Error sending email', err);
       });
   }
 
+  
+
   return {
-    notifier: function(type, user, notifierOptions) {
+    notifier: async function(type, user, notifierOptions) {
       let tokenLink;
       let email;
       switch (type) {
-        case "resendVerifySignup": //sending the user the verification email
-          tokenLink = getLink("verify", user.verifyToken);
-          email = {
-            from: emailConfig.GMAIL,
-            to: user.email,
-            subject: "Verify Signup",
-            html: tokenLink
-          };
-          return sendEmail(email);
-          break;
+      case 'resendVerifySignup': //sending the user the verification email
+        tokenLink = getLink('verify', user.verifyToken);
+        console.info('Email', 'resendVerifySignup', {tokenLink});
+          
+        let emailOpts = {
+          transport
+        };
 
-        case "verifySignup": // confirming verification
-          tokenLink = getLink("verify", user.verifyToken);
-          email = {
-            from: emailConfig.GMAIL,
-            to: user.email,
-            subject: "Confirm Signup",
-            html: tokenLink
-          };
-          return sendEmail(email);
-          break;
+        let sendOpts = {
+          template: 'verify',
+          message: {
+            to: user.email
+          },
+          locals: {
+            link: tokenLink
+          },
+        };
 
-        case "sendResetPwd":
-          tokenLink = getLink("reset", user.resetToken);
-          email = {
-            from: emailConfig.GMAIL,
-            to: user.email,
-            subject: "Confirm reset password",
-            html: tokenLink
-          };
-          return sendEmail(email);
-          break;
+  
+        let testEmail = new Email(emailOpts);
+        
+        try {
+          let result = await testEmail.send(sendOpts);
+          console.info('Email', 'resendVerifySignup', 'success', {result});
+        } catch (e) {
+          console.error('Email', 'resendVerifySignup', 'failed', e);
+        }
+        break;
+        // console.dir({testEmail});
 
-        case "resetPwd":
-          tokenLink = getLink("reset", user.resetToken);
-          email = {};
-          return sendEmail(email);
-          break;
+          // email = {
+          //   from: emailConfig.GMAIL,
+          //   to: user.email,
+          //   subject: "Verify Signup",
+          //   html: tokenLink
+          // };
+        return sendEmail(email);
+        break;
 
-        case "passwordChange":
-          email = {};
-          return sendEmail(email);
-          break;
+      case 'verifySignup': // confirming verification
+        tokenLink = getLink('verify', user.verifyToken);
+        email = {
+          from: emailConfig.GMAIL,
+          to: user.email,
+          subject: 'Confirm Signup',
+          html: tokenLink
+        };
+        return sendEmail(email);
+        break;
 
-        case "identityChange":
-          tokenLink = getLink("verifyChanges", user.verifyToken);
-          email = {};
-          return sendEmail(email);
-          break;
+      case 'sendResetPwd':
+        tokenLink = getLink('reset', user.resetToken);
+        email = {
+          from: emailConfig.GMAIL,
+          to: user.email,
+          subject: 'Confirm reset password',
+          html: tokenLink
+        };
+        return sendEmail(email);
+        break;
 
-        default:
-          break;
+      case 'resetPwd':
+        tokenLink = getLink('reset', user.resetToken);
+        email = {};
+        return sendEmail(email);
+        break;
+
+      case 'passwordChange':
+        email = {};
+        return sendEmail(email);
+        break;
+
+      case 'identityChange':
+        tokenLink = getLink('verifyChanges', user.verifyToken);
+        email = {};
+        return sendEmail(email);
+        break;
+
+      default:
+        break;
       }
     }
   };
