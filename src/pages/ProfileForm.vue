426c8fbf08e6a407113619f7cc9ec8aa38474161
Jonathan Tiritilli <jonathantiritilli@gmail.com>
2019-04-16 11:22:45 -0700

    refactor profile form to use VueX

--- src/pages/ProfileForm.vue
+++ src/pages/ProfileForm.vue
@@ -1,80 +1,81 @@
 <template>
   <q-page :padding="true">
     <h6>Profile Edit Form</h6>
-    
+
     <div>
       <b>Email</b><br/>
-      <q-input v-model="myEmail" type="email"/><br/>
+      <q-input v-model="form.email" type="email"/><br/>
     </div>
     <div>
       <b>Full Name</b><br/>
-      <q-input v-model="myName" /><br/>
+      <q-input v-model="form.name" /><br/>
     </div>
 
     <div>
       <b>Homepage URL</b> (Optional)<br/>
-      <q-input  v-model="myUrl" /><br/>
+      <q-input  v-model="form.url" /><br/>
     </div>
 
     <div>
       <b>Password</b><br/>
-      <q-input  v-model="myPassword" type="password"/>
+      <q-input  v-model="form.password" type="password"/>
     </div>
 
     <div>
       <q-btn label="Submit" @click="doSubmit" /><q-btn label="Cancel" @click="$router.replace('/home')" />
     </div>
-    
+
   </q-page>
 </template>
 <script>
 import api from 'src/api'
 const users = api.service('users')
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   data () {
     return {
-      myId: '',
-      myEmail: '',
-      myName: '',
-      myUrl: '',
-      myPassword: '',
-      thisUser: null
+      form: {
+        id: '',
+        email: '',
+        name: '',
+        url: '',
+        password: ''
+      },
+      user: '',
     }
   },
   methods: {
-    doSubmit () {
-      var usr = this.thisUser
-      var data = {}
-      //usr.email = this.myEmail
-      if (usr.homepage !== this.myUrl) {
-        data.homepage = this.myUrl
+    ...mapActions({
+      getUsers: 'users/get',
+      patchUsers: 'users/patch',
+    }),
+    async doSubmit () {
+      const { homepage, email, fullname, myPassword } = this.user;
+
+      const data = {
+        homepage: this.form.url !== homepage ? this.form.url : homepage,
+        email: this.form.email !== email ? this.form.email : email,
+        fullname: this.form.name !== fullname ? this.form.name : fullname,
+        password: this.form.password !== password ? this.form.password : password,
       }
-      if (usr.email !== this.myEmai) {
-        data.email = this.myEmail
+      try {
+        await this.patchUsers(usr._id, data);
+        this.$router.push('/home');
+      } catch (e) {
+        console.error('profileForm', 'doSubmit', 'error', e);
       }
-      if (usr.fullname !== this.myName) {
-        data.fullname = this.myName
-      }
-      if (this.myPassword !== '') {
-        data.password = this.myPassword
-      }
-      users.patch(usr._id, data)
-          .then ((response) => {
-            this.$router.push('/home')
-      })
     }
   },
-  mounted () {
-    this.$store.commit('questView', false)
-    this.myId = this.$route.params.id
-    users.get(this.myId)
-      .then((user) => {
-        this.thisUser = user
-        this.myEmail = user.email
-        this.myName = user.fullname
-        this.myUrl = user.homepage // likely doesn't exist
-      })
+  async mounted () {
+    this.$store.commit('questView', false);
+    const user_id = this.$route.params.id;
+    const user = await this.getUsers(user_id);
+    this.user = user;
+    this.form.user = user_id;
+    this.form.password = user.email;
+    this.form.name = user.fullname;
+    this.form.url = user.homepage || ''; // likely doesn't exist
   }
 }
 </script>
